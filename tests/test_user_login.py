5084c9fc3547d97656590f87d99bc1e200a4fcd1
Hui Lin Tay <s10178003@connect.np.edu.sg>
2019-12-18 07:37:05 +0800

    migrate to create database

--- tests/test_user_login.py
+++ tests/test_user_login.py
@@ -7,11 +7,19 @@ import pytest
 # Setup
 driver = webdriver.Chrome()
 
+
 # 1 Test access to login page
-
-
 def test_access():
-    driver.get('http://localhost:8000/admin')
+    driver.get("http://localhost:8000/admin")
     assert "Log in | Django site admin" in driver.title
 
-#
+
+# 2 Test login
+# 2.1 Test invalid username
+def test_invalid_username():
+    driver.get("http://localhost:8000/admin")
+    assert "Log in | Django site admin" in driver.title
+
+
+# 2.2 Test invalid password
+# 2.3 Test valid username and password
